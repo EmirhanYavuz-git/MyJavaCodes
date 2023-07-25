43f624f11333c926eec958949891dc2077178205
EmirhanYavuz-git <yavuzemirhan222@gmail.com>
2023-07-25 17:34:19 +0300

    Enum printing

---
+++ m2-enums/src/Currency.java
@@ -0,0 +1,4 @@
+public enum Currency {
+
+    PENNY,NICKLE,DIME,QUARTER
+}
---
+++ m2-enums/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("How to access a constant");
+
+        Currency c = Currency.DIME;
+        System.out.println(c);
+
+
+
+    }
+}
