3028286a8ce4cd0dc660a06ddb83180aac595374
EmirhanYavuz-git <yavuzemirhan222@gmail.com>
2023-08-03 16:26:17 +0300

    Lombok @Data feature

---
+++ m3-lambda/src/main/java/User.java
@@ -0,0 +1,14 @@
+import lombok.*;
+
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+}
