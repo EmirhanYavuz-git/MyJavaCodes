f3d4248e2e70f76dcaaf1ea34e55ce69d0b77f46
EmirhanYavuz-git <yavuzemirhan222@gmail.com>
2023-08-13 16:28:51 +0300

    Min max and parallel stream

--- m6-stream/src/main/java/com/emirhan/FindingMatching.java
+++ m6-stream/src/main/java/com/emirhan/FindingMatching.java
@@ -3,6 +3,9 @@ package com.emirhan;
 import com.emirhan.task.Dish;
 import com.emirhan.task.DishData;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -42,6 +45,26 @@ public class FindingMatching {
         //PARALLEL STREAMS (Async)
         System.out.println(IntStream.range(0,100).parallel().findAny());
         System.out.println(IntStream.range(0,100).parallel().findFirst());
+
+        List<String> list1 = Arrays.asList("Johnny","David","Jack","Duke","Jill","Dany","Julia","Jenish","Divya");
+        List<String> list2 = Arrays.asList("Johnny","David","Jack","Duke","Jill","Dany","Julia","Jenish","Divya");
+
+        Optional<String> findFirst = list1.parallelStream().filter(s -> s.startsWith("D")).findFirst();
+        Optional<String> findAny = list1.parallelStream().filter(s -> s.startsWith("J")).findAny();
+
+        System.out.println(findFirst.get());
+        System.out.println(findAny.get());
+
+        System.out.println("MIN");
+        Optional<Dish> dMin = DishData.getAll().stream().min(Comparator.comparing(Dish::getCalories));
+        System.out.println(dMin.get());
+
+        System.out.println("MAX");
+        Optional<Dish> dMax = DishData.getAll().stream().max(Comparator.comparing(Dish::getCalories));
+        System.out.println(dMax);
+
+
+
 
 
 
