77e33feec2568f2f55a7e435e1d4da84da90e0b0
EmirhanYavuz-git <yavuzemirhan222@gmail.com>
2023-08-15 16:11:03 +0300

    Java 9 updates

---
+++ m8-new-version-updates/src/main/java/com/emirhan/java9/CompactStringDemo.java
@@ -0,0 +1,11 @@
+package com.emirhan.java9;
+
+public class CompactStringDemo {
+
+    public static void main(String[] args) {
+
+        String abc = "cydeo";
+
+        //New coder latin1 implemented to increase performance by decreasing memory usage.
+    }
+}
