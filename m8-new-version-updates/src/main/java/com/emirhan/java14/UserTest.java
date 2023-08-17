bb551b7417bb13f38117b502c841d7f79e956a40
EmirhanYavuz-git <yavuzemirhan222@gmail.com>
2023-08-17 13:54:32 +0300

    Java 14 record

---
+++ m8-new-version-updates/src/main/java/com/emirhan/java14/UserTest.java
@@ -0,0 +1,11 @@
+package com.emirhan.java14;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+        User user = new User("mike","VA");
+        System.out.println(user.name());
+        System.out.println(user.toString());
+    }
+}
